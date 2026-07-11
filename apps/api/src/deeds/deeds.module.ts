8551240e42459e3bef8db6cc506694a434c2e488
anujshrm325-bit <anujshrm325@gmail.com>
2026-07-11 13:54:37 +0530

    Add DeedDocuments controller and service to DeedsModule

--- apps/api/src/deeds/deeds.module.ts
+++ apps/api/src/deeds/deeds.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { SampleDeedsController } from "./sample-deeds.controller.js";
 import { SampleDeedsService } from "./sample-deeds.service.js";
+import { DeedDocumentsController } from "./deed-documents.controller.js";
+import { DeedDocumentsService } from "./deed-documents.service.js";
 
 @Module({
-  controllers: [SampleDeedsController],
-  providers: [SampleDeedsService],
+  controllers: [SampleDeedsController, DeedDocumentsController],
+  providers: [SampleDeedsService, DeedDocumentsService],
   exports: [SampleDeedsService],
 })
 export class DeedsModule {}
