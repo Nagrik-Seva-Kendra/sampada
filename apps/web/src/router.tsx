21f1b8593e96ea90d58f0a91b0b99ba3f34f9cf0
anujshrm325-bit <anujshrm325@gmail.com>
2026-07-14 19:17:25 +0530

    Render TrustBadges on homepage, right under Hero

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -14,6 +14,7 @@ import { FAQ } from "./features/home/FAQ";
 import { Footer } from "./features/home/Footer";
 import { WhatsAppFab } from "./features/home/WhatsAppFab";
 import { CallNowFab } from "./features/home/CallNowFab";
+import { TrustBadges } from "./features/home/TrustBadges";
 import { DeedsPage } from "./features/deeds/DeedsPage";
 import { DeedDetailPage } from "./features/deeds/DeedDetailPage";
 import { DeedEditPage } from "./features/deeds/DeedEditPage";
@@ -43,6 +44,7 @@ function HomePage() {
   return (
     <>
       <Hero />
+      <TrustBadges />
       <Services />
       <OurStory />
       <Testimonials />
