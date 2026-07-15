3c2a78c86cc65ecab46a16031f8b1ba045146f24
anujshrm325-bit <anujshrm325@gmail.com>
2026-07-15 11:21:40 +0530

    Add 'navPartners' item to navigation

--- apps/web/src/features/home/Nav.tsx
+++ apps/web/src/features/home/Nav.tsx
@@ -13,6 +13,7 @@ import { useChangePassword } from "../profile/useProfile";
 const NAV_ITEMS: { key: StringKey; to: string }[] = [
   { key: "navHome", to: "/" },
   { key: "navContact", to: "/contact" },
+  { key: "navPartners", to: "/partners" },
 ];
 const LANGS: Language[] = ["en", "hi"];
 
