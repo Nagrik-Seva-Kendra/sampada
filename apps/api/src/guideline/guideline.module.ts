225573483cad01336523d14ce93af73356b04ab8
a2zgwl <anujshrm325@gmail.com>
2026-07-18 12:35:59 +0530

    Delete apps/api/src/guideline directory

--- apps/api/src/guideline/guideline.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { GuidelineController } from "./guideline.controller.js";
-import { GuidelineService } from "./guideline.service.js";
-
-@Module({
-    controllers: [GuidelineController],
-    providers: [GuidelineService],
-})
-export class GuidelineModule {}
