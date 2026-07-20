3e1162febe868e393c6f5376f086c8a84abab4ed
Anuj Sharma <anujshrm325@gmail.com>
2026-07-20 20:07:56 +0530

    Register PublicDeedDocumentsController

--- apps/api/src/deeds/deeds.module.ts
+++ apps/api/src/deeds/deeds.module.ts
@@ -4,10 +4,11 @@ import { SampleDeedsService } from "./sample-deeds.service.js";
 import { DeedDocumentsController } from "./deed-documents.controller.js";
 import { DeedDocumentsService } from "./deed-documents.service.js";
 import { PublicDeedsController } from "./public-deeds.controller.js";
+import { PublicDeedDocumentsController } from "./public-deed-documents.controller.js";
 import { DeedLiveService } from "./deed-live.service.js";
 
 @Module({
-  controllers: [SampleDeedsController, DeedDocumentsController, PublicDeedsController],
+  controllers: [SampleDeedsController, DeedDocumentsController, PublicDeedsController, PublicDeedDocumentsController],
   providers: [SampleDeedsService, DeedDocumentsService, DeedLiveService],
   exports: [SampleDeedsService],
 })
